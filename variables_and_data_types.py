f97499ff51629c98c3877427e4d324284415997e
Ömer Bilici <102596640+omerblc@users.noreply.github.com>
2023-09-05 21:38:13 +0300

    variables_and_data_types

---
+++ variables_and_data_types.py
@@ -0,0 +1,8 @@
+character_name = "George"  # string variable type
+character_age = 70  # int variable type
+
+print("There once was a man named " + character_name + ",")
+print("he was " + str(character_age) + " years old.")
+# can't concatenate string to int. I must turn int to string with str() code.
+print("He really liked the name " + character_name + ",")
+print("but didn't like being " + str(character_age) + ".")
